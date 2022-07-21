264da7dc07932a15b7e7f817099478dcd96ca508
TheMattrix <69353441+TheMattrixYT@users.noreply.github.com>
2022-07-21 09:13:47 +0800

    Added files and added initial edits.

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// waiting for app to be ready
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 900
+    });
+    // load html
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'main.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
