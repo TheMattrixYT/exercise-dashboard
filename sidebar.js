806b51dcca9334b34cd54debc855ae3f04f989a4
TheMattrix <69353441+TheMattrixYT@users.noreply.github.com>
2022-07-23 19:29:09 +0800

    Added revert of sidebar (which STILL doesn't work will fix that tomorrow)

--- sidebar.js
+++ sidebar.js
@@ -6,6 +6,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+let menuIconCounter = 0;
 function animMenuIcon(x) {
-    x.classList.toggle("change");
+    if (menuIconCounter % 2 == 0) {
+        x.classList.toggle("change");
+    }
+    else {
+        x.classList.toggle("revert");
+    }
 }
